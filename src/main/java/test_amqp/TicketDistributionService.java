a3d02249d4c3949f0ab7e55dcdc1b9efaa7e47f9
ofadejeva <olgafadejeva11@gmail.com>
2016-05-09 22:42:00 +0100

    Further work

---
+++ src/main/java/test_amqp/TicketDistributionService.java
@@ -0,0 +1,7 @@
+package test_amqp;
+
+/**
+ * Created by olga on 09/05/16.
+ */
+public class TicketDistributionService {
+}
--- src/main/java/test_amqp/queue/QueueListener.java
+++ src/main/java/test_amqp/queue/QueueListener.java
@@ -5,6 +5,7 @@ import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.support.converter.MessageConversionException;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -28,7 +29,12 @@ public class QueueListener {
 
     @RabbitListener(queues = "queue")
     public void onMessage(Message message) {
-       TicketRequest request = (TicketRequest) messageConverter.fromMessage(message);
-        logger.info("Received a ticket request: "  + request);
+        System.out.println("Listening to the queue...");
+        try {
+            TicketRequest request = (TicketRequest) messageConverter.fromMessage(message);
+        } catch (MessageConversionException e) {
+            e.printStackTrace();
+        }
+        //logger.info("Received a ticket request: "  + request);
     }
 }
